902fe3c490ecc1c7271558f628871167f85746e0
aabboud1 <abboudahmed1@gmail.com>
2020-10-07 15:49:25 -0400

    first delvierable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  makeTask();
 });
+
+function makeTask() {
+  let form = document.getElementById('create-task-form');
+  form.addEventListener('submit', makeList);
+}
+
+function makeList(event) {
+  event.preventDefault();
+  // console.log(event.target);
+  let descrption = document.getElementById('new-task-description').value
+  let ul = document.getElementById('tasks');
+  let li = document.createElement('li');
+  ul.appendChild(li);
+  li.innerText = descrption
+  // let li = document.createElement('li');
+  // let rule = document.createTextNode(x);
+  // li.appendChild(rule)
+}
